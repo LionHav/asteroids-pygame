63f1fb6c5c63c57224cc37efc70309ab25ef28d5
LionHav <lion.haverkamp@googlemail.com>
2025-08-20 15:13:51 +0200

    Added player and movement for player

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt=0 #delta time
+    clock=pygame.time.Clock()
 
-    inf_loop=True
-    while inf_loop==True:
+    player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt=clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
